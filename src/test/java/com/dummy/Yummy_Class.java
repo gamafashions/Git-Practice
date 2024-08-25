89f12818ede7b8b7cc4c45fbb0cb2f3d9e86b23b
gamafashions <in.gamafashions@gmail.com>
2024-08-25 13:04:26 +0530

    Adding the Yummy File to the remote branch

---
+++ src/test/java/com/dummy/Yummy_Class.java
@@ -0,0 +1,15 @@
+package com.dummy;
+
+public class Yummy_Class {
+	private void methods() {
+		System.out.println("Methods");
+
+	}
+	private void methodTen() {
+		System.out.println("methodTen");
+
+	}
+
+	
+
+}
